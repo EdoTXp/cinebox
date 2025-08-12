8e050d246a3c6d9fa9f60256b319e2662cba2e40
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2025-08-12 15:40:03 -0300

    feat (package) : Config Riverpod
    - Add Riverpod dependencies;
    - Add ProviderScope to CineboxMainApp;
    - Transform StatefulWidget in ConsumerStatefulWidget for SplashScreen;

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Splash Screen'),
+        title: const Text('Splash Screen Riverpod'),
       ),
       body: Container(),
     );
