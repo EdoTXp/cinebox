30aa27cd5a585ba82eafd25bdbf6a5ce054c3eb7
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2025-08-15 18:14:07 -0300

    feat (project) Add Movie Repository to all usecases
    - Create MarkFavoriteExtension to map movies and add favorites to those who have it;
    - Add MoviesRepository to all usecases and map favorites movies;

--- lib/domain/usecases/get_movies_by_genre_usecase.dart
+++ lib/domain/usecases/get_movies_by_genre_usecase.dart
@@ -1,18 +1,36 @@
 import 'package:cinebox/core/result/result.dart';
+import 'package:cinebox/data/repositories/movies/movies_repository.dart';
 import 'package:cinebox/data/repositories/tmdb/tmdb_repository.dart';
+import 'package:cinebox/domain/extensions/mark_favorite_extension.dart';
+import 'package:cinebox/domain/models/favorite_movie.dart';
 import 'package:cinebox/domain/models/movie.dart';
 
 class GetMoviesByGenreUsecase {
   final TmdbRepository _tmdbRepository;
+  final MoviesRepository _moviesRepository;
 
-  GetMoviesByGenreUsecase({required TmdbRepository tmdbRepository})
-    : _tmdbRepository = tmdbRepository;
+  GetMoviesByGenreUsecase({
+    required TmdbRepository tmdbRepository,
+    required MoviesRepository moviesRepository,
+  }) : _tmdbRepository = tmdbRepository,
+       _moviesRepository = moviesRepository;
 
   Future<Result<List<Movie>>> execute({required int genreId}) async {
-    final moviesByGenreResult = await _tmdbRepository.getMoviesByGenres(
-      genreId: genreId,
-    );
+    final results = await Future.wait([
+      _moviesRepository.getMyFavoritesMovies(),
+      _tmdbRepository.getMoviesByGenres(
+        genreId: genreId,
+      ),
+    ]);
 
-    return moviesByGenreResult;
+    if (results case [
+      Success<List<FavoriteMovie>>(value: final favorites),
+      Success<List<Movie>>(value: final movieByGenre),
+    ]) {
+      final favoriteId = favorites.map((f) => f.id).toList();
+      return Success(movieByGenre.markAsFavorite(favoriteId));
+    }
+
+    return Failure(Exception('Error on get movies by genre'));
   }
 }
--- lib/domain/usecases/usecases_providers.g.dart
+++ lib/domain/usecases/usecases_providers.g.dart
@@ -53,7 +53,7 @@ final class GetMoviesByCategoryUsecaseProvider
 }
 
 String _$getMoviesByCategoryUsecaseHash() =>
-    r'ee1e937b2a6758ba1014ec7b0a329dcbccfeda95';
+    r'84e67e1ae090f69afae985bbefd12e0e68cf6526';
 
 @ProviderFor(getMoviesByGenreUsecase)
 const getMoviesByGenreUsecaseProvider = GetMoviesByGenreUsecaseProvider._();
@@ -101,7 +101,7 @@ final class GetMoviesByGenreUsecaseProvider
 }
 
 String _$getMoviesByGenreUsecaseHash() =>
-    r'd9791832e62d9206423659405e8e26278f8cb537';
+    r'52403765a3169dd10e1cd5ed9c461a3728b01bf6';
 
 @ProviderFor(getMoviesByNameUsecase)
 const getMoviesByNameUsecaseProvider = GetMoviesByNameUsecaseProvider._();
@@ -149,7 +149,7 @@ final class GetMoviesByNameUsecaseProvider
 }
 
 String _$getMoviesByNameUsecaseHash() =>
-    r'ca9cc2af285a193c746fe0fba311484e2f4b0c24';
+    r'fc84570471708977484a5a2cc8e02bd5d4743db5';
 
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
