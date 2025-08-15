7195fa0df899332842bedecdb3a22b8b0caf7766
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2025-08-15 15:45:04 -0300

    feat (project) Integrate Genre to Genre Box
    - Integrate Genre to Genre Box;
    - Add getGenres method to TmdbRepository;
    - Create Genre Domain Model;
    - Create GetGenres Command;
    - Watch the changes in genre in GetGenresCommandProvider and return widget based on state;

--- lib/data/repositories/tmdb/tmdb_repository_impl.dart
+++ lib/data/repositories/tmdb/tmdb_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:cinebox/core/result/result.dart';
 import 'package:cinebox/data/exceptions/data_exception.dart';
 import 'package:cinebox/data/mappers/movie_mappers.dart';
 import 'package:cinebox/data/services/tmdb/tmdb_service.dart';
+import 'package:cinebox/domain/models/genre.dart';
 
 import 'package:cinebox/domain/models/movie.dart';
 import 'package:dio/dio.dart';
@@ -85,6 +86,26 @@ class TmdbRepositoryImpl implements TmdbRepository {
     } on DioException catch (e, s) {
       log('Error on get Up Coming Movies', error: e, stackTrace: s);
       return Failure(DataException(message: 'Error on get Up Coming Movies'));
+    }
+  }
+
+  @override
+  Future<Result<List<Genre>>> getGenres() async {
+    try {
+      final data = await _tmdbService.getMoviesGenres();
+      final genres = data.genres
+          .map(
+            (g) => Genre(
+              id: g.id,
+              name: g.name,
+            ),
+          )
+          .toList();
+
+      return Success(genres);
+    } on DioException catch (e, s) {
+      log('Error to get Genres', error: e, stackTrace: s);
+      return Failure(DataException(message: 'Error to get Genres'));
     }
   }
 }
