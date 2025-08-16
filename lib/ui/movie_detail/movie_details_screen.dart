2d3bcce143ac0edc2124fcaf0afae9862177a6a7
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2025-08-16 09:39:49 -0300

    feat (project) Add Movie Detail Screen
    - Add Movie Detail Screen route to CineboxMainApp;
    -  Add InkWell to MovieCard for redirect to Detail Screen;
    -  Add Movie Detail Screen;
    - Create Cast Box Widget;

---
+++ lib/ui/movie_detail/movie_details_screen.dart
@@ -0,0 +1,90 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cinebox/ui/core/themes/colors.dart';
+import 'package:cinebox/ui/core/themes/text_styles.dart';
+import 'package:cinebox/ui/movie_detail/widgets/cast_box.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_stars/flutter_rating_stars.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MovieDetailsScreen extends ConsumerStatefulWidget {
+  const MovieDetailsScreen({super.key});
+
+  @override
+  ConsumerState<MovieDetailsScreen> createState() => _MovieDetailScreenState();
+}
+
+class _MovieDetailScreenState extends ConsumerState<MovieDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalhes do Filme'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 24,
+          children: [
+            SizedBox(
+              height: 278,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: CachedNetworkImage(
+                      imageUrl:
+                          'https://img.elo7.com.br/product/zoom/2A1A4BF/big-poster-filme-joker-coringa-joaquin-phoenix-lo02-90x60-cm-poster.jpg',
+                      placeholder: (context, url) => Container(
+                        width: 160,
+                        color: AppColors.lightGrey,
+                        child: const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 8,
+                children: [
+                  Text(
+                    'Coringa',
+                    style: AppTextStyles.titleLarge,
+                  ),
+                  RatingStars(
+                    starCount: 5,
+                    starColor: AppColors.yellow,
+                    starSize: 14,
+                    valueLabelVisibility: false,
+                    value: 3.5,
+                  ),
+                  Text(
+                    'Gêneros',
+                    style: AppTextStyles.lightGreyRegular,
+                  ),
+                  Text(
+                    'Ano (Local) | Hora',
+                    style: AppTextStyles.regularSmall,
+                  ),
+                  Text(
+                    'Sinopse',
+                    style: AppTextStyles.regularSmall,
+                  ),
+                  CastBox(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
